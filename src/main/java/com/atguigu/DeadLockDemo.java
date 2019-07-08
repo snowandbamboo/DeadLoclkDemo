6eb48ef13de76e4fbe2ed473eb14e472b11f66d5
snowandbamboo <1215134782@qq.com>
2019-07-08 20:43:54 +0800

    DeadLockDemo

---
+++ src/main/java/com/atguigu/DeadLockDemo.java
@@ -0,0 +1,36 @@
+package com.atguigu;
+
+import java.util.concurrent.TimeUnit;
+
+class HoldLockThread implements Runnable{
+    private String lockA;
+    private String lockB;
+
+    public HoldLockThread(String lockA, String lockB) {
+        this.lockA = lockA;
+        this.lockB = lockB;
+    }
+
+    @Override
+    public void run() {
+        synchronized (lockA){
+            System.out.println(Thread.currentThread().getName()+"\t 自己持有"+lockA+" \t 尝试获得"+ lockB);
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            }catch (Exception e){
+
+            }
+            synchronized (lockB){
+                System.out.println(Thread.currentThread().getName()+"\t 自己持有lockB \t 尝试获得lockA");
+            }
+        }
+    }
+}
+public class DeadLockDemo {
+    public static void main(String[] args) {
+        String lockA = "lockA";
+        String lockB = "lockB";
+        new Thread(new HoldLockThread(lockA,lockB),"ThreadAAA").start();
+        new Thread(new HoldLockThread(lockB,lockA),"ThreadBBB").start();
+    }
+}
